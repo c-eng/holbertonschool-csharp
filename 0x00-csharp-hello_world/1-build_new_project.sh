efe964ed04dacec77a12d8df26a058b13d348511
c-eng <ceng@cca.edu>
2019-09-09 16:08:11 -0700

    Adding shell refreshers

---
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dotnet new console -o 1-new_project
+dotnet build 1-new_project
---
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+dotnet new console -o 2-new_project
+dotnet build 2-new_project
+dotnet run --project ./2-new_project
