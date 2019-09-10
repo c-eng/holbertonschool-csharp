3184e26ca8eabfd84d2e3b73939e2b022c60a589
c-eng <ceng@cca.edu>
2019-09-09 20:46:16 -0700

    Fix:Removing String Literals

--- 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
-		str = str.Substring(str.IndexOf("object"), 15) + str.Substring(str.IndexOf("program") - 1, 13) + str.Substring(str.IndexOf("in "), 3) + str.Substring(0,2);
+		str = str.Substring(str.IndexOf(49, 15) + str.Substring(79, 13) + str.Substring(119, 3) + str.Substring(0,2));
 		Console.WriteLine(str);
         }
 }
