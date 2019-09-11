757c24e9efbd71113d041fc22236bb53bfa7ef4e
c-eng <ceng@cca.edu>
2019-09-10 19:06:23 -0700

    Initial commit of projects

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char letter = 'a'; letter <='z'; letter++)
+            {
+                if (letter != 'e' && letter != 'q')
+                    Console.Write("{0}", letter);
+            }
+        }
+    }
+}
