3afdd33d4fa38e1dccb57f7f9d20ef04ebec6531
c-eng <heirvuyar@gmail.com>
2019-09-12 14:08:13 -0700

    New: Adding optional task

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [][] jagger = new int[][]
+        {
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0,1}
+        };
+        for (int i = 0; i < jagger.Length; i++)
+        {
+            for (int j = 0; j < jagger[i].Length; j++)
+            {
+                if (j != 0)
+                    Console.Write(" ");
+                Console.Write(jagger[i][j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
