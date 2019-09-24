30ab6edf786d99aad9b9d594b05ce1a063cc2a21
c-eng <heirvuyar@gmail.com>
2019-09-24 15:06:18 -0700

    Fix: Adding null list case

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -5,6 +5,8 @@ class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
+        if (myLList == null)
+            return (0);
         int num = myLList.First.Value;
         myLList.RemoveFirst();
         return (num);
