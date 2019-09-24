c7c18a3295671db92e1a5f51fd2e761cd050fe1a
c-eng <heirvuyar@gmail.com>
2019-09-24 15:02:13 -0700

    New: Adding linked list tasks

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int num = myLList.First.Value;
+        myLList.RemoveFirst();
+        return (num);
+    }
+}
