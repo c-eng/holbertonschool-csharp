b4a487ad381da6dec54f041d9e46ea203cbc64c7
c-eng <heirvuyar@gmail.com>
2019-09-26 16:33:22 -0700

    New: Adding health field and constructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents an Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Health field of a zombie.</summary>
+        public int health;
+        ///<summary>Constructor for Zombie.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
