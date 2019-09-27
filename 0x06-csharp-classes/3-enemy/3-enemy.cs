81bff7524f0ee46e3ec931508b3e05404f3b4bdc
c-eng <heirvuyar@gmail.com>
2019-09-26 17:40:55 -0700

    New: Added get method for health variable

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents an Zombie.</summary>
+    public class Zombie
+    {
+        //<summary>Health field of a zombie.</summary>
+        private int health;
+        ///<summary>Constructor for Zombie.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Constructor with health value input for Zombie</summary>
+        public Zombie (int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        ///<summary>Method to return current health of a zombie</summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}
