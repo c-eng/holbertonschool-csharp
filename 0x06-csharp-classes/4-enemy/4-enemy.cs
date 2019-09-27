0d086f2f756318a19a132f7c0ce7df0071e072e3
c-eng <heirvuyar@gmail.com>
2019-09-26 17:46:02 -0700

    New: Adding name/getter/setter

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents an Zombie.</summary>
+    public class Zombie
+    {
+        //<summary>Health field of a zombie.</summary>
+        private int health;
+        //<summary>Name field of a zombie</summary>
+        private string name = "(No name)";
+        ///<summary>Constructor for Zombie.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Constructor with health value input for Zombie</summary>
+        public Zombie (int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        ///<summary>Method to return current health of a zombie</summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+        ///<summary>Gets or sets the name.</summary>
+        public string Name
+        {
+            get {return (name);}
+            set {name = value;}
+        }
+    }
+}
