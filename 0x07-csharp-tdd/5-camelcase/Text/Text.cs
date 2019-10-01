474a6308383b0c31ed96cc38d1d780e329f5fcb6
c-eng <heirvuyar@gmail.com>
2019-10-01 13:53:28 -0700

    New: Adding tasks and tests

---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Text
+{
+    ///<summary>Performs operations on text string inputs.</summary>
+    public class Str
+    {
+        ///<summary>Counts words in a camelCase string.</summary>
+        public static int CamelCase(string s)
+        {
+            int count = 0;
+            if (s.Length != 0)
+                count++;
+            foreach (char daffy in s)
+            {
+                if (Char.IsUpper(daffy))
+                    count++;
+            }
+            return (count);
+        }
+    }
+}
