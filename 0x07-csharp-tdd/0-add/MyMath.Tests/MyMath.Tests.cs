8d01fca43ff3b810daba91a4383cf6329fa7eeb3
c-eng <heirvuyar@gmail.com>
2019-10-03 15:05:52 -0700

    Fix: Reverting tests

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -2,18 +2,32 @@ using NUnit.Framework;
 
 namespace MyMath.Tests
 {
-	[TestFixture]
-	public class OperationsTests
-	{
-		[Test]
-		public void AddTest1()
-		{
-			int input_a = 5;
-			int input_b = 6;
-
-			int output = Operations.Add(input_a, input_b);
-
-			Assert.AreEqual(input_a + input_b, output);
-		}
-	}
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Add_Two_Positive()
+        {
+            int a = 90;
+            int b = 8;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+        [Test]
+        public void Add_Positive_Negative()
+        {
+            int a = -2;
+            int b = 100;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+        [Test]
+        public void Add_Two_Negative()
+        {
+            int a = -8;
+            int b = -90;
+            int c = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, c);
+        }
+    }
 }
