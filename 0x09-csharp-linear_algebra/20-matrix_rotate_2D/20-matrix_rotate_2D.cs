a7be8b8cff9b3ea7e60a3fbb785ced6b790e7d1e
c-eng <heirvuyar@gmail.com>
2019-10-23 20:56:38 -0700

    Fix: Reverting previous change

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,5 +1,4 @@
-﻿/*
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 ///<summary>Algebraic operations on matricies.</summary>
@@ -27,44 +26,9 @@ class MatrixMath
                 {
                     temp += rotate[i, k] * matrix[k, j];
                 }
-                matrix3[i, j] = Math.Round(temp);
+                matrix3[i, j] = Math.Round(temp, 2);
             }
         }
         return (matrix3);
     }
-}
-*/
-using System;
-
-///<summary>Class for matrix math</summary>
-class MatrixMath
-{
-	///<summary>calcuating matrix rotation</summary>
-	///<return>the result matrix or matrix containing -1</return>
-	public static double[,] Rotate2D(double[,] matrix, double angle)
-	{
-		double[,] rotation = {
-			{Math.Cos(angle), -Math.Sin(angle)},
-			{Math.Sin(angle), Math.Cos(angle)}
-		};
-		int m_row = matrix.GetLength(0);
-		int m_col = matrix.GetLength(1);
-
-		if (2 == m_row)
-		{
-			double[,] result = new double[2, m_col];
-			for (int i = 0; i < m_row; i++)
-			{
-				for (int j = 0; j < 2; j++)
-				{
-					for (int k = 0; k < m_col; k++)
-					{
-						result[i, j] += Math.Round(rotation[i, k] * matrix[k, j]);
-					}
-				}
-			}
-			return result;
-		}
-		return new double[,] { { -1 } };
-	}
 }
