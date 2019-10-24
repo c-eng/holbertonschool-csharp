4d06905a489f3640377f4ea9a7b6abbd7a30b504
c-eng <heirvuyar@gmail.com>
2019-10-23 20:29:15 -0700

    Fis: Testing known good example

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 
 ///<summary>Algebraic operations on matricies.</summary>
@@ -31,4 +32,39 @@ class MatrixMath
         }
         return (matrix3);
     }
+}
+*/
+using System;
+
+///<summary>Class for matrix math</summary>
+class MatrixMath
+{
+	///<summary>calcuating matrix rotation</summary>
+	///<return>the result matrix or matrix containing -1</return>
+	public static double[,] Rotate2D(double[,] matrix, double angle)
+	{
+		double[,] rotation = {
+			{Math.Cos(angle), -Math.Sin(angle)},
+			{Math.Sin(angle), Math.Cos(angle)}
+		};
+		int m_row = matrix.GetLength(0);
+		int m_col = matrix.GetLength(1);
+
+		if (2 == m_row)
+		{
+			double[,] result = new double[2, m_col];
+			for (int i = 0; i < m_row; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					for (int k = 0; k < m_col; k++)
+					{
+						result[i, j] += Math.Round(rotation[i, k] * matrix[k, j]);
+					}
+				}
+			}
+			return result;
+		}
+		return new double[,] { { -1 } };
+	}
 }
