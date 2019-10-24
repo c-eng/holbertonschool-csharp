ea961fdcc301d0ba8e0e351b9cd6ce324d907807
c-eng <heirvuyar@gmail.com>
2019-10-23 19:47:12 -0700

    New: Adding task

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Algebraic operations on matricies.</summary>
+class MatrixMath
+{
+    ///<summary>Rotate a matrix.</summary>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] bad = new double[,] {{-1}};
+        double[,] rotate = new double[2, 2] {{Math.Cos(angle), Math.Sin(angle)},
+                                               {-1 * Math.Sin(angle), Math.Cos(angle)}};
+        double[,] matrix3 = new double[2, 2];
+        double temp;
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return (bad);
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                temp = 0;
+                for (int k = 0; k < 2; k++)
+                {
+                    temp += matrix[i, k] * rotate[k, j];
+                }
+                matrix3[i, j] = Math.Round(temp, 2);
+            }
+        }
+        return (matrix3);
+    }
+}
